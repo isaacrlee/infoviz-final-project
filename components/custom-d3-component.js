91916c8289e81f00a140239d7143d3ff9cc87767
Isaac Lee <iasacrlee@gmail.com>
2019-05-29 18:00:25 -0500

    First pass at big 10 chart and pie chart
    Co-authored-by: Jeremy Kaish <jeremykaish@u.northwestern.edu>
    Co-authored-by: jurassicali <jessicali1.2018@u.northwestern.edu>

--- components/custom-d3-component.js
+++ components/custom-d3-component.js
@@ -3,27 +3,75 @@ const D3Component = require('idyll-d3-component');
 const d3 = require('d3');
 
 const size = 600;
+var height = 500;
+var width = 600;
+var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+  width = 600 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+var schools = { "OSU": "Ohio State University", "MSU": "Michigan State University", "IU": "Indiana University", "UMich": "University of Michigan", "UIUC": "University of Illinois Urbana Champaign", "UI": "University of Iowa", "NU": "Northwestern University", "PSU": "Pennsylvania State University", "RU": "Rutgers University", "UMinn": "University of Minnesota", "PU": "Purdue University", "UW": "University of Wisconsin", "UN": "University of Nebraska" }
 
 class CustomD3Component extends D3Component {
 
   initialize(node, props) {
     const svg = this.svg = d3.select(node).append('svg');
     svg.attr('viewBox', `0 0 ${size} ${size}`)
-      .style('width', '100%')
-      .style('height', 'auto');
+      // .style('width', '100%')
+      // .style('height', 'auto')
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+    var x = d3.scaleBand()
+      .range([0, width])
+      .padding(0.1);
+    var y = d3.scaleLinear()
+      .range([height, 0]);
 
-    svg.append('circle')
-      .attr('r', 20)
-      .attr('cx', Math.random() * size)
-      .attr('cy', Math.random() * size);
+    var div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+    var data = props.data;
+    x.domain(data.map(function (d) { return d.x; }));
+    y.domain([0, 37307]);
+    svg.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar").attr("fill", "steelblue")
+      .attr("x", function (d) { return x(d.x); })
+      .attr("width", x.bandwidth())
+      .attr("y", function (d) { return y(d.y); })
+      .attr("height", function (d) { return height - y(d.y); })
+      .on("mouseover", function (d) {
+
+        div.transition()
+          .duration(100)
+          .style("opacity", .9);
+        div.html("School: " + schools[d.x] + "<br> Avg Loans: $" + d.y)
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function (d) {
+        div.transition()
+          .duration(500)
+          .style("opacity", 0);
+      });
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // add the y Axis
+
+    svg.append("g")
+      .call(d3.axisLeft(y));
   }
 
   update(props, oldProps) {
-    this.svg.selectAll('circle')
-      .transition()
-      .duration(750)
-      .attr('cx', Math.random() * size)
-      .attr('cy', Math.random() * size);
+    // this.svg.selectAll('circle')
+    //   .transition()
+    //   .duration(750)
+    //   .attr('cx', Math.random() * size)
+    //   .attr('cy', Math.random() * size);
   }
 }
 
